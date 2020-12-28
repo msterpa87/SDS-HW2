c49bcdf6f3fe37f0a13bf98f79d68918cb0af666
msterpa87 <marcosterpa1@gmail.com>
2020-12-28 18:47:41 +0100

    EM-fit on generic number of mixtures

--- generic_comp_mixture.R
+++ generic_comp_mixture.R
@@ -1,24 +1,13 @@
-M   <- 100
-# M   <- 1000
-out <- rep(NA, M)
-for (m in 1:M){
-  lab = sample(c("Female", "Male"), size = 1, prob = c(.4,.6))
-  if (lab == "Female") 
-    out[m] <- rnorm(n = 1, mean = 162, sd = sqrt(5))
-  else 
-    out[m] <- rnorm(n = 1, mean = 176, sd = sqrt(8))
-}
-hist(out, main = "", xlab = "")
-
 # Handmade EM4MoG ---------------------------------------------------------
 
-vnorm <- function(p, mu, sigma, y) p * dnorm(y, mu, sigma)
+vnorm <- function(y, p, mu, sigma) p * dnorm(y, mu, sigma)
 vect_wnorm <- Vectorize(vnorm, c("p", "mu", "sigma"))
+vect_norm = Vectorize(dnorm, c("mu", "sigma"))
 
 handmade.em <- function(y, p, mu, sigma, n_iter, plot_flag = T)
 {
   # vectorized likelihood
-  like     <- vect_wnorm(y, p, mu, sigma)
+  like     <- apply(vect_wnorm(y, p, mu, sigma), 1, sum)
   deviance <- -2 * sum(log(like))
   
   # sampling plot colors
@@ -38,16 +27,14 @@ handmade.em <- function(y, p, mu, sigma, n_iter, plot_flag = T)
     r   <- apply(d, 2, function(x) x / den) # normalizing each column
     
     # M step
-    p[1]     <- mean(r1)
-    mu[1]    <- sum(r1*y)/sum(r1)
-    sigma[1] <-sqrt( sum(r1*(y^2))/sum(r1) - (mu[1])^2 )
-    p[2]     <- 1 - p[1]
-    mu[2]    <- sum((r2)*y)/sum((r2))
-    sigma[2] <- sqrt(sum(r2*(y^2))/sum(r2) - (mu[2])^2)
+    p <- apply(r, 2, mean)
+    mu <- apply(r, 2, function(x) sum(x * y) / sum(x))
+    sigma <- apply(r, 2, function(x) sum(x * (y^2)) / sum(x))
+    sigma <- sqrt(sigma - mu^2)
     
     # -2 x log-likelihood (a.k.a. deviance)
-    like     <- p[1]*dnorm(y, mu[1], sigma[1]) + p[2]*dnorm(y, mu[2], sigma[2])
-    deviance <- -2*sum( log(like) )
+    like     <- apply(vect_wnorm(y, p, mu, sigma), 1, sum)
+    deviance <- -2 * sum(log(like))
     
     # Save
     res[iter+1,] <- c(iter, p, mu, sigma, deviance)
@@ -57,16 +44,34 @@ handmade.em <- function(y, p, mu, sigma, n_iter, plot_flag = T)
       hist(y, prob = T, breaks = 30, col = gray(.8), border = NA, 
            main = "", xlab = paste("EM Iteration: ", iter, "/", n_iter, sep = ""))
       set.seed(123)
+      # compute max component for each obs
+      norms <- vect_norm(y, mu, sigma)
+      cols_idxs <- apply(norms, 1, which.max)
+      curve_fun <- function(x) sum(vect_wnorm(x, p, mu, sigma))
+      
       points(jitter(y), rep(0,length(y)), 
              pch = 19, cex = .6, 
-             col = cols[ (dnorm(y,mu[1],sigma[1]) > dnorm(y,mu[2],sigma[2])) + 1])
-      curve(p[1]*dnorm(x,mu[1],sigma[1]) + p[2]*dnorm(x,mu[2],sigma[2]),
+             col = cols[cols_idxs])
+      curve(sapply(x, curve_fun),
             lwd = 4, col = rgb(0,0,0,.5), add = TRUE)
       Sys.sleep(1.5)
     }
   }
   res <- data.frame(res)
-  names(res) <- c("iteration","p1","p2","mu1","mu2","sigma1","sigma2","deviance")
+  # function to generate strings
+  append_str <- function(str) sapply(1:n, function(x) paste(str, x, sep=''))
+  p_names <- append_str("p")
+  mu_names <- append_str("mu")
+  sigma_names <- append_str("sigma")
+  names(res) <- c("iteration", p_names, mu_names, sigma_names, "deviance")
   out <- list(parameters = c(p = p, mu = mu, sigma = sigma), deviance = deviance, res = res)
   return(out)
 }
+
+data("faithful")
+?faithful
+hem_fit <- handmade.em(faithful$waiting, 
+                       p      = c(.5,.2,.3), 
+                       mu     = c(45,55,65), 
+                       sigma  = c(8,8,8), 
+                       n_iter = 5)
