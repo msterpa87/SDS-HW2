caf3b832747cd7b8037f34c7672135ec1b8df70b
msterpa87 <marcosterpa1@gmail.com>
2020-12-31 15:20:04 +0100

    implemented m step with iteration (works)

--- generic_comp_mixture.R
+++ generic_comp_mixture.R
@@ -6,19 +6,37 @@ library(KScorrect)
 
 vnorm <- function(y, p, mu, sigma) p * dnorm(y, mu, sigma)
 vect_wnorm <- Vectorize(vnorm, c("p", "mu", "sigma"))
-vect_norm = Vectorize(dnorm, c("mu", "sigma"))
+vect_norm = Vectorize(dnorm, c("mean", "sd"))
 
-handmade.em <- function(y, p, mu, sigma, n_iter = 20, plot_flag = F)
-{
-  # vectorized likelihood
-  like     <- apply(vect_wnorm(y, p, mu, sigma), 1, sum)
-  deviance <- -2 * sum(log(like))
-  
+plot_fit <- function(y, p, mu, sigma, xlab = "", breaks = 50) {
   # sampling plot colors
   color_list <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
   k    <- length(p)
   cols <- sample(color_list, k)
+  
+  hist(y, prob = T, breaks = breaks, col = gray(.8), border = NA, 
+       main = "", xlab = xlab)
+  set.seed(123)
+  # compute max component for each obs
+  norms <- vect_norm(y, mu, sigma)
+  cols_idxs <- apply(norms, 1, which.max)
+  curve_fun <- function(x) sum(vect_wnorm(x, p, mu, sigma))
+  
+  points(jitter(y), rep(0,length(y)), 
+         pch = 19, cex = .6, 
+         col = cols[cols_idxs])
+  curve(sapply(x, curve_fun),
+        lwd = 4, col = rgb(0,0,0,.5), add = TRUE)
+  Sys.sleep(1.5)
+}
 
+handmade.em <- function(y, p, mu, sigma, breaks = 50, n_iter = 20, plot = F)
+{
+  # vectorized likelihood
+  like     <- apply(vect_wnorm(y, p, mu, sigma), 1, sum)
+  deviance <- -2 * sum(log(like))
+  k <- length(p)
+  
   # creating matrix of iteration values
   n_col    <- 2 + 3 * k
   res      <- matrix(NA, n_iter + 1, n_col)
@@ -28,13 +46,20 @@ handmade.em <- function(y, p, mu, sigma, n_iter = 20, plot_flag = F)
     # E step
     d   <- vect_wnorm(y, p, mu, sigma) # matrix of responsabilities
     den <- apply(d, 1, sum) # normalizing factor
-    r   <- apply(d, 2, function(x) x / den) # normalizing each column
-    
+    r <- d / den # normalizing each column
+
     # M step
     p <- apply(r, 2, mean)
-    mu <- apply(r, 2, function(x) sum(x * y) / sum(x))
-    sigma <- apply(r, 2, function(x) sum(x * (y^2)) / sum(x))
-    sigma <- sqrt(sigma - mu^2)
+    #mu <- apply(r, 2, function(x) sum(x * y) / sum(x))
+    for (i in 1:k) {
+      mu[i] <- sum(r[,i] * y) / sum(r[,i])
+    }
+
+    for (i in 1:k) {
+      sigma[i] <- sqrt(sum(r[,i] * (y - mu[i])^2) / sum(r[,i]))
+    }
+    #sigma <- apply(r, 2, function(x) sum(x * (y^2) ) / sum(x))
+    #sigma <- sqrt(sigma - mu^2)
     
     # -2 x log-likelihood (a.k.a. deviance)
     like     <- apply(vect_wnorm(y, p, mu, sigma), 1, sum)
@@ -44,24 +69,13 @@ handmade.em <- function(y, p, mu, sigma, n_iter = 20, plot_flag = F)
     res[iter+1,] <- c(iter, p, mu, sigma, deviance)
     
     # Plot
-    if (plot_flag){
-      hist(y, prob = T, breaks = 30, col = gray(.8), border = NA, 
-           main = "", xlab = paste("EM Iteration: ", iter, "/", n_iter, sep = ""))
-      set.seed(123)
-      # compute max component for each obs
-      norms <- vect_norm(y, mu, sigma)
-      cols_idxs <- apply(norms, 1, which.max)
-      curve_fun <- function(x) sum(vect_wnorm(x, p, mu, sigma))
-      
-      points(jitter(y), rep(0,length(y)), 
-             pch = 19, cex = .6, 
-             col = cols[cols_idxs])
-      curve(sapply(x, curve_fun),
-            lwd = 4, col = rgb(0,0,0,.5), add = TRUE)
-      Sys.sleep(1.5)
+    if (plot) {
+      xlab <- paste("EM Iteration: ", iter, "/", n_iter, sep = "")
+      plot_fit(y, p, mu, sigma, xlab)
     }
   }
   res <- data.frame(res)
+  
   # function to generate strings
   append_str <- function(str) sapply(1:k, function(x) paste(str, x, sep=''))
   p_names <- append_str("p")
@@ -92,6 +106,8 @@ n_breaks = 100
 #hist(sample_sim(n_2), prob=T, breaks=n_breaks)
 
 log_likelihood <- function(x, hm.fit) {
+  # x : vector
+  # hm.fit : output of handmade.em function
   like <- apply(vect_wnorm(x, hm.fit$p, hm.fit$mu, hm.fit$sigma), 1, sum)
   log_like <- sum(log(like)) # log likelihood
   return(log_like)
@@ -103,9 +119,22 @@ init_params <- function(k) {
   # initial parameters, data are used to compute min/max to
   # generate mu and sigma
   p <- runif(k, 0, 1)
-  p <- p / sum(p) # normalizing
-  mu <- runif(k, -1, 1)
+  p <- p / sum(p)
+  mu <- runif(k, 0, 1)
   sigma <- runif(k, 0, 1)
+  return(list(p = p, mu = mu, sigma = sigma))
+}
+
+init_params_groups <- function(y, k) {
+  # takes the data and the number of components and returns random
+  # initial parameters, data are used to compute min/max to
+  # generate mu and sigma
+  folds <- createFolds(y, k) # group of indexes
+  folds <- rapply(folds, function(x) y[x], how = "replace") # groups of values
+  p <- rep(1 / k, k) # same proportion for each component
+  p <- p / sum(p) # normalizing
+  mu <- as.vector(rapply(folds, mean)) # mean of each random group
+  sigma <- as.vector(rapply(folds, sd))
   return(list(p = p, mu = mu, sigma = sigma))
 }
 
@@ -148,7 +177,9 @@ kfold_cv <- function(XX, k, em_run) {
   return(mean(outs))
 }
 
-EM_simulation <- function(n, M, k_max, n_iter = 20, verbose = T) {
+print_params <- function(p, mu, sigma) cat("\np =", p, "\nmu =", mu, "\nsigma =", sigma, "\n")
+
+EM_simulation <- function(n, M, k_max, n_iter = 20, subdivisions = 200, verbose = T) {
   # n : sample size
   # M : number of simulations
   # k_max : maximum number of components
@@ -168,7 +199,6 @@ EM_simulation <- function(n, M, k_max, n_iter = 20, verbose = T) {
     
     for (k in 1:k_max) {
       params <- init_params(k) # initial random parameters
-      
       # hem.fit instantiated with fixed parameters to make multiple calls within the loop
       em_run <- function(x) handmade.em(x, params$p, params$mu, params$sigma, n_iter)
       
@@ -177,17 +207,17 @@ EM_simulation <- function(n, M, k_max, n_iter = 20, verbose = T) {
       
       # AIC and BIC evaluation
       log_like <- log_likelihood(XX, hm.fit)
-      model_eval[1,k] <- 2 * (log_like - k)
-      model_eval[2,k] <- 2 * log_like - log(n) * k
+      model_eval[1,k] <- 2 * (k * 3 - log_like)
+      model_eval[2,k] <- log(n) * k * 3 - 2 * log_like
       
       # cross validation on different split ratios
       ratios = c(.5, .7, .3)
       
-      for (i in 1:length(ratios)) {
+      for (i in 1:3) {
         x_split <- sample_split(XX, ratios[i])
         hm.fit <- em_run(x_split$train)
         n_test <- length(x_split$test)
-        log_like <- 1/n_test * log_likelihood(x_split$test, hm.fit)
+        log_like <- 1 / n_test * log_likelihood(x_split$test, hm.fit)
         model_eval[i+2,k] <- log_like
       }
       
@@ -207,12 +237,14 @@ EM_simulation <- function(n, M, k_max, n_iter = 20, verbose = T) {
       test_ecdf <- ecdf(x_split$test) # ecdf of the test set
       f_test <- function(p) as.numeric(quantile(test_ecdf, probs = p)) # quantile function of testset ecdf
       f_abs <- function(p) abs(f_k(p) - f_test(p)) # integrand
-      model_eval[8,k] <- integrate(f_abs, lower = 0, upper = 1, subdivisions = 500)$value
+      #model_eval[8,k] <- integrate(f_abs, lower = 0, upper = 1, subdivisions = subdivisions)$value
+      model_eval[8,k] <- 0
     }
 
     # take best k for each mode evaluation and save it in the result matrix
-    print(model_eval)
-    result[,n_sim] <- unlist(apply(model_eval, 1, which.max))
+    model_eval[is.nan(model_eval)] = Inf # eliminating possible NaN values
+    if (verbose) print(model_eval)
+    result[,n_sim] <- unlist(apply(model_eval, 1, which.min))
   }
   end_time <- Sys.time()
   total_time <- end_time - start_time
@@ -223,8 +255,8 @@ EM_simulation <- function(n, M, k_max, n_iter = 20, verbose = T) {
   return(result)
 }
 
-accuracy <- function(k_matrix) {
-  result <- unlist(apply(k_matrix, 1, function(x) sum(x == 6) / length(x)))
+accuracy <- function(k_matrix, k) {
+  result <- unlist(apply(k_matrix, 1, function(x) sum(x == k) / length(x)))
   names(result) <- c("AIC", "BIC", "50/50-SS", "70/30-SS", "30/70-SS", "5-Fold-CV", "10-Fold-CV", "Wasserstein")
   return(result)
 }
